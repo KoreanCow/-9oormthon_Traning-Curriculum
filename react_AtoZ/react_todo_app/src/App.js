b9b8260ac99e51e2dcfb1ce493324b2a2c993c72
KoreanCow <>
2023-12-26 17:35:11 +0900

    add tailwindcss dnd functional_Component

--- react_AtoZ/react_todo_app/src/App.js
+++ react_AtoZ/react_todo_app/src/App.js
@@ -1,97 +1,49 @@
-import React , {Component}  from'react';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Lists from "./components/Lists";
+import Form from "./components/Form";
 
-export default class App extends Component {
-  
-  state = {
-    todoData : [
-      {
-        id: '1',
-        title: '공부하기',
-        completed: true,
-      },
-      {
-        id: '2',
-        title: '청소하기',
-        completed: false,
-      }
-    ],
-    value: '',
-  }
-  btnStyle = {
-    color: '#fff',
-    border: 'none',
-    padding: '5px 9px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right',
-  }
+function App() {
+  const [todoData, setTodoData] = useState([]);
 
-  getStyle = () => {
-    return{
-      padding: '10px',
-      borderBottom: '1px #ccc dotted ',
-      textDecoration: 'none',
-    }
-  }
+  const [value, setValue] = useState("");
 
-  handleCLick = (id) => {
-    let newTodoData = this.state.todoData.filter(data => data.id !== id);
-    
-    this.setState({todoData: newTodoData});
-  }
+  const handleSubmit = (e) => {
+    // form 안에 input을 전송할 때 페이지 리로드 되는 걸 막아줌
+    e.preventDefault();
 
-  handleChange = (e) => {
-    this.setState({value: e.target.value});
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault(); 
-
+    // 새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
-    this.setState({todoData: [...this.state.todoData, newTodo]});
-  }
+    // 원래 있던 할 일에 새로운 할 일 더해주기
+    setTodoData((prev) => [...prev, newTodo]);
 
-  render() {
-    return (
-      <div className='container'>
-        <div className='todoBlock'>
-          <div className='title'>
-            <h1>할 일 목록</h1>
-          </div>
 
-          {this.state.todoData.map((data) => {
-            return(
-              <div style={this.getStyle()} key={data.id}>
-                <input type='checkbox' defaultChecked={data.completed} />
-                {data.title}
-                <button style={this.btnStyle} onClick={() => this.handleCLick(data.id)}>X</button>
-              </div>
-            )
-            })}  
-            <form style={{display: 'flex'}} onSubmit={this.handleSubmit}>
-              <input 
-                type='text' 
-                name='value'
-                style={{ flex: '10', padding: '5px 9px'}} 
-                placeholder='해야 할 일을 입력하세요.'
-                value={this.state.value}
-                onChange={this.handleChange}
-                />
-              <input
-                type='submit'
-                value='입력'
-                className='btn'
-                style={{flex: '1', }}
-              />
-            </form>
+    // 입력란에 있던 글씨 지워주기
+    setValue("");
+  };
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
+        <Lists todoData={todoData} setTodoData={setTodoData} />
+
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
-    )
-  }
+    </div>
+  );
 }
+
+export default App;
